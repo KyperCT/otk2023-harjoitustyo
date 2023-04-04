d3a00aae87b99ca994ec678f8ee9c913cd9fc66a
inanimateobject <mirko.vonbell@helsinki.fi>
2023-04-04 18:40:27 +0300

    setup invoke, pytest, coverage

---
+++ tetris/tasks.py
@@ -0,0 +1,15 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python src/main.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage html", pty=True)
