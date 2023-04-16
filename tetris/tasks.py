a1128e13eea736ec2e1f0cb6052f02d33d33c265
inanimateobject <mirko.vonbell@helsinki.fi>
2023-04-16 15:08:36 +0300

    added alternative command for report

--- tetris/tasks.py
+++ tetris/tasks.py
@@ -15,5 +15,10 @@ def coverage(ctx):
     ctx.run("coverage html", pty=True)
 
 @task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage html", pty=True)
+
+@task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
