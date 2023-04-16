0682b8c1f035c0c92e9017aa316a0771a6e713d6
inanimateobject <mirko.vonbell@helsinki.fi>
2023-04-16 13:52:56 +0300

    refactored main loop

--- tetris/src/main.py
+++ tetris/src/main.py
@@ -1,7 +1,7 @@
 import pygame
 from gui import display, interaction
 from model import tetris_grid, tetris_block
-from logic import movement, score
+from logic import movement, score, core
 
 
 def main():
@@ -17,8 +17,7 @@ def main():
 
     running = True
     tick_speed = 10
-    game_speed = 4
-    state = 0
+    state = None
 
     blocks = [tetris_block.Block()]
     total_score = 0
@@ -28,15 +27,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         keys = interaction.get_keypresses()
-        movement.user_move(grid, keys, blocks)
-        if state == game_speed:
-            movement.game_move(grid, blocks)
-            state = 0
-        else:
-            state += 1
-        if len(blocks) == 0:
-            total_score = score.check_score(grid, total_score)
-            blocks.append(tetris_block.Block())
+        grid, total_score, state = core.core_loop(grid, total_score, keys, state)
         display.render(grid, total_score, main_display, resolution, font)
         pygame.display.update()
         clock.tick(tick_speed)
